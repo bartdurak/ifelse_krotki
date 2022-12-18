edf5b9c53a002fa11fdf7df0c3b88a60a3f8c543
bartdurak <projektator@gmail.com>
2022-12-18 21:51:25 +0100

    4. ifelse

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+//krótki if else
+    int a;
+    cout<<"Podaj wartosc liczbowa A = ";
+    cin>>a;
+    if (a < 0)
+        cout<<"Podales liczbe ujemna";
+    else
+    if (a==0)
+        cout<<"Podales cyfre zero ";
+    else
+        cout<<"Podales liczbe dodatnia";
+
+return 0;
+}
